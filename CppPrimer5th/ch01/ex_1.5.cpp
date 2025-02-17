a5bed7c6a2fcdd7bdd2d02ff45ac6b6ecaf5235a
qwli7 <228794754@qq.com>
2025-02-17 20:17:00 +0800

    Create ex_1.5.cpp

---
+++ CppPrimer5th/ch01/ex_1.5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int a;
+    int b;
+    std::cin >> a >> b;
+    std::cout << "The sum of ";
+    std::cout << a;
+    std::cout << " and ";
+    std::cout << b;
+    std::cout << " is ";
+    std::cout << a + b << std::endl;
+    return -1;
+}
