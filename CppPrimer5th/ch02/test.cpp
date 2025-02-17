cb379e8c83a5094da2109ca54b4388c02b1b6f46
qwli7 <228794754@qq.com>
2025-02-18 00:41:15 +0800

    add ch02

---
+++ CppPrimer5th/ch02/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+int main()
+{
+    float a = 3.14f;
+    double b = 10 / 3;
+
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+    return 0;
+}
