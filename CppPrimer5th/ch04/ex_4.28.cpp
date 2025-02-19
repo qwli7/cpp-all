b3cb17f32447389a38eaebbd223d69b924f3fb85
qwli7 <228794754@qq.com>
2025-02-19 15:54:42 +0800

    Create ex_4.28.cpp

---
+++ CppPrimer5th/ch04/ex_4.28.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+using std::endl;
+int main()
+{
+
+    cout << sizeof(short) << endl;
+    cout << sizeof(int) << endl;
+    cout << sizeof(unsigned int) << endl;
+    cout << sizeof(long int) << endl;
+    cout << sizeof(long long int) << endl;
+    cout << sizeof(double) << endl;
+    cout << sizeof(float) << endl;
+    cout << sizeof(char) << endl;
+    cout << sizeof(bool) << endl;
+    cout << sizeof(long double) << endl;
+    cout << sizeof(int *) << endl;
+    cout << sizeof(double *) << endl;
+    cout << sizeof(float *) << endl;
+    cout << sizeof(char *) << endl;
+    return 0;
+}
