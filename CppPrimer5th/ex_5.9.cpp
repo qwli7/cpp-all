bd9ed74499c2e7756dc16ae0d1cdd77f51b3c6b8
qwli7 <228794754@qq.com>
2025-02-19 17:37:44 +0800

    Create ex_5.9.cpp

---
+++ CppPrimer5th/ex_5.9.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+int main()
+{
+    int a_cnt = 0;
+    int o_cnt = 0;
+    int e_cnt = 0;
+    int i_cnt = 0;
+    int u_cnt = 0;
+    string str;
+    cin >> str; // 读取一个字符串，用空格分隔
+    // getline(cin, str); //读取一行
+    for (auto c : str)
+    {
+        switch (c)
+        {
+        case 'a':
+            a_cnt++;
+            break;
+        case 'o':
+            o_cnt++;
+            break;
+        case 'u':
+            u_cnt++;
+            break;
+        case 'e':
+            e_cnt++;
+            break;
+        case 'i':
+            i_cnt++;
+            break;
+        default:
+            break;
+        }
+    }
+    cout << "a_cnt: " << a_cnt << endl;
+    cout << "o_cnt: " << o_cnt << endl;
+    cout << "u_cnt: " << u_cnt << endl;
+    cout << "e_cnt: " << e_cnt << endl;
+    cout << "i_cnt: " << i_cnt << endl;
+
+    return 0;
+}
