f8c74125a7dd2f279aebd18257f0cc3df72afefb
qwli7 <228794754@qq.com>
2025-02-20 15:55:28 +0800

    Update Chapter6.h

--- CppPrimer5th/ch06/Chapter6.h
+++ CppPrimer5th/ch06/Chapter6.h
@@ -1,9 +1,9 @@
 #ifndef __Chapter6_h__
 #define __Chapter6_h__
 
-long long int fact(int n);
-long long int fact2(int);
+long long fact(int n);
+long long fact2(int);
 
-int abs(int n);
+int newabs(int n);
 int abs2(int);
 #endif
