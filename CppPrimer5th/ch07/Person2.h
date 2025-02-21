dde9259be706d2bc497e47b531e6e16136483790
qwli7 <228794754@qq.com>
2025-02-21 12:36:08 +0800

    Create Person2.h

---
+++ CppPrimer5th/ch07/Person2.h
@@ -0,0 +1,26 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+typedef struct Person
+{
+    std::string name;
+    std::string address;
+    std::string get_name() const
+    {
+        return name;
+    };
+    std::string get_address() const
+    {
+        return address;
+    }
+
+} p;
+
+// 读取
+std::istream &read(std::istream &is, p &person);
+
+// 打印
+std::ostream &print(std::ostream &os, const p &person);
+
+#endif
