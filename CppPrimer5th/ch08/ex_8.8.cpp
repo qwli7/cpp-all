556d7706f861fdd2a98137981a903ac040453e4c
qwli7 <228794754@qq.com>
2025-02-24 19:13:21 +0800

    Create ex_8.8.cpp

---
+++ CppPrimer5th/ch08/ex_8.8.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+int main(int argc, char *argv[])
+{
+    if (argc < 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " filename outputfilename" << std::endl;
+        return -1;
+    }
+    std::string filename(argv[1]);
+    std::ifstream file(filename);
+    if (!file.is_open())
+    {
+        std::cerr << "open file " << filename << " failed!" << std::endl;
+        return -1;
+    }
+    std::string outputfilename(argv[2]);
+    std::ofstream outputfile(outputfilename, std::ios::out | std::ios::app);  //添加追加条件
+    if (!outputfile.is_open())
+    {
+        std::cerr << "open file " << outputfilename << " failed!" << std::endl;
+        file.close();
+        return -1;
+    }
+    std::string line;
+    while (std::getline(file, line))
+    {
+        outputfile << line << endl;
+        cout << line << endl;
+    }
+    file.close();
+    outputfile.close();
+    return 0;
+}
