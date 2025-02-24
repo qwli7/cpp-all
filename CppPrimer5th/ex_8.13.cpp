106cf19ac0c04a304e9d143208c45860d5ea360f
qwli7 <228794754@qq.com>
2025-02-24 19:45:08 +0800

    Create ex_8.13.cpp

---
+++ CppPrimer5th/ex_8.13.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::vector;
+
+struct PersonInfo
+{
+    std::string name;
+    std::vector<std::string> phones;
+};
+int main()
+{
+    string line, word;
+    vector<PersonInfo> persons;
+    std::istringstream record;
+
+    std::ifstream infile("data/person.txt");
+
+    while (std::getline(infile, line))
+    {
+        PersonInfo info;
+        record.str(line);
+        record >> info.name; //读取每一行的第一个单词，作为姓名
+        while (record >> word)
+        {
+            info.phones.push_back(word);
+        }
+        persons.push_back(info);
+        record.clear();
+    }
+    infile.close(); //关闭流
+
+    for (auto person : persons)
+    {
+        for (auto phone : person.phones)
+        {
+            cout << person.name << " " << phone << endl;
+        }
+    }
+
+    return 0;
+}
